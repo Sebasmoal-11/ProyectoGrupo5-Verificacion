408183126a093361b3d50dc86f100043a9ee6fef
RODY JOSUE MEJIA MARTINEZ <rjmejiam@edu.uia.ac.cr>
2025-08-10 23:50:41 -0600

    corregir pruba unitaria HomeControllerUnitTest

--- PruebasUnitarias.UnitTests/Home/HomeControllerUnitTest.cs
+++ PruebasUnitarias.UnitTests/Home/HomeControllerUnitTest.cs
@@ -1,49 +1,33 @@
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Moq;
-using TiendaVirtualMVC.Controllers;
-using WebApplication1.Controllers;
-//Ajusta el namespace del modelo si es diferente
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ControllerNS = WebApplication1.Controllers;
 using ProductModel = TiendaVirtualMVC.Models.Product;
 
 namespace PruebasUnitarias.UnitTests.Home
 {
-    public interface IProductRepository
-    {
-        IEnumerable<ProductModel> GetAll();
-    }
-
     [TestClass]
     public class HomeControllerUnitTest
     {
         [TestMethod]
-        [TestCategory("Funcionalidad Index en HomeController")]
         public async Task IndexDevuelveListaProductos_ModelCorrecto()
         {
-            // Arrange: mock del repo retornando una lista
-            var repo = new Mock<IProductRepository>();
-            repo.Setup(r => r.GetAll()).Returns(new List<ProductModel>
-            {
-                new() { Name = "Mouse", Description = "USB", Price = 10m }
-            });
+            var logger = new Mock<ILogger<ControllerNS.HomeController>>();
+            var controller = new ControllerNS.HomeController(logger.Object); // 👈 SOLO 1 arg
 
-            var controller = new HomeController(repo.Object); // <-- ctor: HomeController(IProductRepository repo)
-
-            // Act (sync/async)
-            IActionResult result;
             var call = controller.Index();
-            result = call is Task<IActionResult> t ? await t : (IActionResult)call;
+            var result = call is Task<IActionResult> t ? await t : (IActionResult)call;
 
-            // Assert
             var view = result as ViewResult;
             Assert.IsNotNull(view, "Index debe retornar un ViewResult.");
-            Assert.IsInstanceOfType(view!.Model, typeof(IEnumerable<ProductModel>));
+            Assert.IsNotNull(view!.Model, "Index debe pasar la lista a View(model).");
 
-            var lista = (IEnumerable<ProductModel>)view.Model!;
-            Assert.IsTrue(lista.Any(), "Se esperaba al menos un producto en la lista.");
+            Assert.IsInstanceOfType(view.Model!, typeof(IEnumerable<ProductModel>),
+                $"El modelo debe ser IEnumerable<{typeof(ProductModel).FullName}>.");
         }
     }
 }
+
